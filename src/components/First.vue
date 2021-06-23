836a567c32be1d5c482856920605855d13c4696f
莫明达 <1031892437@qq.com>
2021-06-24 00:49:35 +0800

    不能在本机上跑有点难受

--- src/components/First.vue
+++ src/components/First.vue
@@ -8,13 +8,13 @@
     <!-- 获取视频信息 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="7">
+        <el-col :span="10">
           <el-input placeholder="请输入视频Id" v-model="videoId" clearable @clear="getVideo">
             <el-button slot="append" icon="el-icon-search" @click="getVideo">获取视频信息</el-button>
           </el-input>
         </el-col>
         <el-col :span="5">
-          <div class="video_Time">该视频的持续时间为：{{this.duration1}}</div>
+          <div class="video_Time">该视频的持续时间为：{{this.video.duration1}}</div>
         </el-col>
       </el-row>
     </el-card>
@@ -22,7 +22,7 @@
     <!-- 获取问题信息 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="7">
+        <el-col :span="10">
           <el-input placeholder="请输入问题Id" v-model="problemId" clearable @clear="getQuestion">
             <el-button slot="append" icon="el-icon-search" @click="getQuestion">获取问题信息</el-button>
           </el-input>
@@ -35,7 +35,7 @@
     <!-- 获取学生信息 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="7">
+        <el-col :span="10">
           <el-input placeholder="请输入学生Id" v-model="studentId" clearable @clear="getStudent">
             <el-button slot="append" icon="el-icon-search" @click="getStudent">获取学生信息</el-button>
           </el-input>
@@ -56,7 +56,7 @@
     <!-- 获取所有视频信息 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="7">
+        <el-col :span="10">
           <el-button type="primary" icon="el-icon-search" @click="getVideoAll">获取所有视频信息</el-button>
         </el-col>
       </el-row>
@@ -79,58 +79,22 @@
     <div style="height: 50px"></div>
     <!-- 获取所有问题信息 -->
     <el-card>
-      <!-- <el-row :gutter="20">
-        <el-col :span="7">
+      <el-row :gutter="20">
+        <el-col :span="10">
           <el-button type="primary" icon="el-icon-search" @click="getQuestionAll">获取所有问题信息</el-button>
         </el-col>
       </el-row>
-      <el-table :data="problems" border stripe>
-        <el-table-column type="index"></el-table-column>
-        <el-table-column label="问题Id" prop="problemId"></el-table-column>
-        <el-table-column label="答题总次数" prop="all"></el-table-column>
-        <el-table-column label="答对次数" prop="right"></el-table-column>
-        <el-table-column label="答错次数" prop="wrong"></el-table-column>
-        <el-table-column label="问题知识点" prop="concept"></el-table-column>
-        <el-table-column label="问题先修知识" prop="preConcept"></el-table-column>
-      </el-table>
-      <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="queryInfo2.current"
-      :page-sizes="[20, 30, 40, 50]"
-      :page-size="queryInfo2.size"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-      </el-pagination> -->
       <div id="main2" style="width: 1000px; height: 800px"></div>
     </el-card>
     <div style="height: 50px"></div>
     <!-- 获取所有学生信息 -->
     <el-card>
       <el-row :gutter="20">
-        <el-col :span="7">
+        <el-col :span="10">
           <el-button type="primary" icon="el-icon-search" @click="getStudentAll">获取所有学生信息</el-button>
         </el-col>
       </el-row>
-      <el-table :data="students" border stripe>
-        <el-table-column type="index"></el-table-column>
-        <el-table-column label="学生Id" prop="studentId"></el-table-column>
-        <el-table-column label="学生掌握知识点" prop="conceptGrasp"></el-table-column>
-        <el-table-column label="学生大致掌握知识点" prop="conceptAlmostGrasp"></el-table-column>
-        <el-table-column label="学生未掌握知识点" prop="conceptNoGrasp"></el-table-column>
-        <el-table-column label="学生总答题目数" prop="all"></el-table-column>
-        <el-table-column label="学生正确题目数" prop="right"></el-table-column>
-      </el-table>
-      <!-- 分页区域 -->
-      <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="queryInfo3.current"
-      :page-sizes="[20, 30, 40, 50]"
-      :page-size="queryInfo3.size"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="studentTotal">
-      </el-pagination>
+      <div id="main3" style="width: 1000px; height: 800px"></div>
     </el-card>
   </div>
 </template>
@@ -208,7 +172,7 @@ export default {
         current: 1,
         size: 20
       },
-      totalOfProblem: '',
+      totalOfProblem: 1,
       problems: [
         {
           problemId: 'P_01c72494e56b41ddba14524d3e4e0ab7',
@@ -246,14 +210,14 @@ export default {
       option2: {
         tooltip: {},
         legend: {
-          data: ['销量']
+          data: ['正确率']
         },
         xAxis: {
           data: this.problems
         },
         yAxis: {},
         series: [{
-          name: '销量',
+          name: '正确率',
           type: 'bar',
           data: [5, 20, 36, 10, 10, 20]
         }]
@@ -264,7 +228,28 @@ export default {
         size: 20
       },
       studentTotal: '',
-      students: []
+      students: [],
+      option3: {
+        parallelAxis: [
+          { dim: 0, name: '正确率' },
+          { dim: 1, name: '错误率' },
+          { dim: 2, name: '未掌握知识点' },
+          { dim: 3, name: '大致掌握知识点' },
+          { dim: 4, name: '掌握知识点' }
+        ],
+        series: {
+          type: 'parallel',
+          lineStyle: {
+            width: 4
+          },
+          data: [
+            [0.6, 0.4, 0.5, 0.2, 0.3],
+            [0.4, 0.6, 0.7, 0.15, 0.15],
+            [0.1, 0.9, 0.4, 0.4, 0.2]
+          ]
+        }
+      },
+      i: 0
     }
   },
   created () {},
@@ -290,7 +275,7 @@ export default {
       this.problem.concept = res.data.problem.concept
       this.problem.preConcept = res.data.problem.preConcept
       console.log(this.problem)
-      this.myChart1 = echarts.init(document.getElementById('main2'))
+      this.myChart1 = echarts.init(document.getElementById('main1'))
       this.myChart1.setOption({
         title: {
           subtext: '问题总次数为：' + res.data.problem.all + '问题涉及知识点' + res.data.problem.concept + '问题先修知识点' + res.data.problem.preConcept
@@ -310,12 +295,12 @@ export default {
       const { data: res } = await this.$http.get('getStudentTable', { params: { studentId: this.studentId } })
       if (res.code !== 200) return this.$message.error(res.data.提示)
       this.$message.success(res.message)
-      this.student.studentId = res.data.student.studentId
-      this.student.conceptGrasp = res.data.student.conceptGrasp
-      this.student.conceptAlmostGrasp = res.data.student.conceptAlmostGrasp
-      this.student.conceptNoGrasp = res.data.student.conceptNoGrasp
-      this.student.all = res.data.student.all
-      this.student.right = res.data.student.right
+      this.student[0].studentId = res.data.student.studentId
+      this.student[0].conceptGrasp = res.data.student.conceptGrasp
+      this.student[0].conceptAlmostGrasp = res.data.student.conceptAlmostGrasp
+      this.student[0].conceptNoGrasp = res.data.student.conceptNoGrasp
+      this.student[0].all = res.data.student.all
+      this.student[0].right = res.data.student.right
       console.log(this.student)
     },
     async getVideoAll() {
@@ -332,7 +317,8 @@ export default {
       if (res.code !== 200) return this.$message.error(res.data.提示)
       this.$message.success(res.message)
       this.totalOfProblem = res.data.totalOfProblem
-      this.videos = res.data.videos
+      this.problems = res.data.problems
+      this.myChart2 = echarts.init(document.getElementById('main2'))
     },
     async getStudentAll() {
       console.log(this.queryInfo)
@@ -343,6 +329,7 @@ export default {
       this.students = res.data.students
       console.log(this.total)
       console.log(this.students)
+      this.myChart3 = echarts.init(document.getElementById('main3'))
     }
   },
   // 此时页面上的元素，已经被渲染完毕了
@@ -351,6 +338,8 @@ export default {
     myChart1.setOption(this.option)
     var myChart2 = echarts.init(document.getElementById('main2'))
     myChart2.setOption(this.option2)
+    var myChart3 = echarts.init(document.getElementById('main3'))
+    myChart3.setOption(this.option3)
   }
 }
 </script>
